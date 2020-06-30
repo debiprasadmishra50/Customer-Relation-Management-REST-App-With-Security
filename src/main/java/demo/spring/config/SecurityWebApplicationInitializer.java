e0cd0494d3e19f484ca1ffd3367ed1a990dae847
debiprasadmishra50 <debiprasadmishra50@gmail.com>
2020-06-30 18:17:37 +0530

    Spring REST CRM Security

---
+++ src/main/java/demo/spring/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package demo.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
